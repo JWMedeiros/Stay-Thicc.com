0fc0e301574b2b889de022beb8aaa7d6d10d6953
unknown <pross2252@gmail.com>
2022-06-29 14:34:54 -0400

    added seed files

---
+++ seeds/seed.js
@@ -0,0 +1,11 @@
+const seedLocations = require("./locationData");
+const seedWorkouts = require("./workoutData");
+const seedUsers = require("./userData");
+
+const seedAll = async () => {
+    await seedLocations();
+    await seedWorkouts();
+    await seedUsers();
+};
+
+seedAll();
