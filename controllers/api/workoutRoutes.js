e0d962416a3d6b592fa1b354182feabc354dd375
liangying <wingkyleung1208@hotmail.com>
2022-07-05 10:53:58 -0400

    css

--- controllers/api/workoutRoutes.js
+++ controllers/api/workoutRoutes.js
@@ -24,8 +24,8 @@ router.delete('/:id', withAuth, async (req, res) => {
     //Destroy specified workout
     //let result=
     await Workout.destroy({
-      where:{
-        id:req.params.id,
+      where: {
+        id: req.params.id,
       },
     });
   } catch (err) {
@@ -33,42 +33,63 @@ router.delete('/:id', withAuth, async (req, res) => {
   }
 });
 
-router.put('/:id', withAuth, async (req, res) => {
-  try {
-    Workout.update({
-      name: req.body.name,
-      weekday: req.body.weekday,
-      type: req.body.type,
-      workout_weight: req.body.type,
-      reps: req.body.reps,
-      sets: req.body.sets,
-    },
-    {
-      where:{
-        id:req.params.id,
-      }
-    })
-      .then((dbUserData) => {
-        if (!dbUserData) {
-          res.status(404).json({ message: 'No Workout found with this id' });
-          return;
-        }
-        res.json(dbUserData);
-      });
-    // // Get all workouts
-    // const workoutData = await Workout.findAll({});
+// router.put("/:id", withAuth, (req, res) => {
+//   Workout.update(req.body, {
+//     where: {
+//       id: req.params.id
+//     }
+//   })
+//     .then(affectedRows => {
+//       if (affectedRows > 0) {
+//         res.status(200).end();
+//       } else {
+//         res.status(404).end();
+//       }
+//     })
+//     .catch(err => {
+//       res.status(500).json(err);
+//     });
+// });
 
-    // // Serialize data so the template can read it
-    // const workouts = workoutData.map((workout) => workout.get({ plain: true }));
 
-    // // Pass serialized data and session flag into template
-    // res.render('workout', {
-    //   workouts,
-    //   loggedIn: req.session.loggedIn,
-    // });
-  } catch (err) {
-    res.status(500).json(err);
-  }
-});
+// router.put('/:id', withAuth, async (req, res) => {
+//   try {
+//     Workout.update({
+//       name: req.body.name,
+//       weekday: req.body.weekday,
+//       type: req.body.type,
+//       workout_weight: req.body.type,
+//       reps: req.body.reps,
+//       sets: req.body.sets,
+//     },
+//       {
+//         where: {
+//           id: req.params.id,
+//         }
+//       })
+//       .then((dbUserData) => {
+//         if (!dbUserData) {
+//           res.status(404).json({ message: 'No Workout found with this id' });
+//           return;
+//         }
+//         res.json(dbUserData);
+//       });
 
-module.exports = router;
+
+// // Get all workouts
+// const workoutData = await Workout.findAll({});
+
+// // Serialize data so the template can read it
+// const workouts = workoutData.map((workout) => workout.get({ plain: true }));
+
+// // Pass serialized data and session flag into template
+// res.render('workout', {
+//   workouts,
+//   loggedIn: req.session.loggedIn,
+// });
+//   } catch (err) {
+//   res.status(500).json(err);
+// }
+// });
+
+module.exports = router
